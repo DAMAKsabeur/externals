d1627daba70510d092aa710a075cde57b42f0cfd
Sabeur DAMAK <damak.sabeur@gmail.com>
2023-05-03 20:56:12 +0200

    update

---
+++ package/s32gx-mkimage/s32gx-mkimage.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# s32gx-mkimage
+#
+################################################################################
+S32GX_MKIMAGE_VERSION = bsp34.2-2020.04
+S32GX_MKIMAGE_SITE = $(call github,nxp-auto-linux,u-boot,$(S32GX_MKIMAGE_VERSION))
+S32GX_MKIMAGE_LICENSE = GPL-2.0+
+S32GX_MKIMAGE_LICENSE_FILES = COPYING
+HOST_S32GX_MKIMAGE_DEPENDENCIES = host-zlib
+
+# i.MX8M needs a different binary
+define HOST_S32GX_MKIMAGE_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) $(HOST_CONFIGURE_OPTS) \
+		CFLAGS="$(HOST_CFLAGS) -std=c99" \
+		-C $(@D)/tools -f Makefile mkimage
+endef
+
+define HOST_S32GX_MKIMAGE_INSTALL_CMDS
+	$(INSTALL) -D -m 755 $(@D)/iMX8M/mkimage_s32gx $(HOST_DIR)/bin/mkimage_s32gx
+	$(INSTALL) -D -m 755 $(@D)/iMX8M/mkimage_fit_atf.sh $(HOST_DIR)/bin/mkimage_fit_atf.sh
+	$(INSTALL) -D -m 755 $(@D)/iMX8M/print_fit_hab.sh $(HOST_DIR)/bin/print_fit_hab.sh
+endef
+# i.MX8 and i.MX8X
+#define HOST_S32GX_MKIMAGE_BUILD_CMDS
+#	$(HOST_MAKE_ENV) $(MAKE) $(HOST_CONFIGURE_OPTS) \
+#		CFLAGS="$(HOST_CFLAGS) -std=c99" \
+#		-C $(@D) all MKIMG=mkimage_s32gx
+#endef
+define HOST_S32GX_MKIMAGE_INSTALL_CMDS
+	$(INSTALL) -D -m 755 $(@D)/mkimage $(HOST_DIR)/bin/mkimage_s32gx
+endef
+
+$(eval $(host-generic-package))
